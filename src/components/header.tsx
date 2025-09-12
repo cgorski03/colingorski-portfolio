3f41f97a5e5b39f7cf843765560577666bbfdc61
Colin Gorski <cgorski03@gmail.com>
2025-09-12 13:58:29 -0400

    scorecard draft

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function Header() {
+	return (
+		< header className="text-center mb-16" >
+			<h1 className="text-4xl md:text-6xl font-bold text-foreground tracking-wider mb-4">
+				COLIN GORSKI - SOFTWARE ENGINEER
+			</h1>
+			<h2 className="text-xl md:text-3xl text-muted-foreground tracking-wide mb-2">THE 18 HOLES OF CODE</h2>
+			<p className="text-lg md:text-xl text-muted-foreground tracking-wide">NAVIGATING THE FAIRWAYS OF CAREER.</p>
+		</header >
+	)
+}
---
+++ src/types/hole.ts
@@ -0,0 +1,9 @@
+export type ScorecardHole = {
+	id: number;
+	name: string;
+	subtitle: string;
+	tech: string[];
+	par: number;
+	description: string;
+	difficulty: string;
+}
