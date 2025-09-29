95bea36bf8cfe3c74457eb1cea2c348708c11729
Colin Gorski <cgorski03@gmail.com>
2025-09-29 18:06:20 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,18 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Colin Gorski",
   description: "Colin Gorski interactive resume",
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' }
+    ],
+    apple: '/apple-touch-icon.png',
+    other: [
+      { rel: 'icon', url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { rel: 'icon', url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }
+    ]
+  },
 };
 
 export default function RootLayout({
